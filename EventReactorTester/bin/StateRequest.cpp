0def5cf158585cc72ebca33f36640f6cb0bf018d
malakuti <somayeh.malakuti@gmail.com>
2016-01-07 13:24:42 +0100

    removed Tester

--- EventReactorTester/bin/StateRequest.cpp
+++
@@ -1,9 +0,0 @@
-#include "StateRequest.h"
-#include <string>
-using namespace std;
-StateRequest ::StateRequest () {
-setType("StateRequest");
-		addDynamicContext("ping", "boolean");
-		addDynamicContextVal("ping", "false");
-		setBase("BaseEventType");
-}
